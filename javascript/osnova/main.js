d0bdac47a78185337e30afc8b611aadd08f0eb67
whynur <dauletova.ainur2801@gmail.com>
2023-03-10 01:33:03 +0600

    bitlab

---
+++ javascript/osnova/main.js
@@ -0,0 +1,37 @@
+function task1() {
+    let x = parseInt(prompt("Insert a number"));
+    let y = ((x*x) + 2*x -1);
+    alert(y);
+}
+
+function task2() {
+    let a = parseInt(prompt("Insert a:"));
+    let b = parseInt(prompt("Insert b:"));
+    if (a % b == 0) {
+        alert("Divisible")
+    }
+    else {
+        alert("Not Divisible")
+    }
+}
+
+function task3() {
+    let name = prompt("Insert your name");
+    let surname = prompt("Insert your surname");
+    let city = prompt("Insert your city");
+    let age = prompt("Insert your age");
+
+    alert("Name: " + name + '\n' + " Surname: " + surname);
+
+}
+task3();
+function task4() {
+    let a = parseInt(prompt("Insert a:"));
+    let b = parseInt(prompt("Insert b:"));
+    let c = parseInt(prompt("Insert c:"));
+
+}
+
+function task5() {
+
+}
